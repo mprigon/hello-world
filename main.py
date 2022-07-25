2c21864d37874b7e8e93e5cf77ce66e3b11e7af5
Maksim Prigon <mprigon@yandex.ru>
2022-07-25 12:44:44 +0300

    add main.py decorator

---
+++ main.py
@@ -0,0 +1,21 @@
+# декоратор, в котором встроенная функция умеет принимать аргументы
+def do_it_twice(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+        print('декоратор отработал')
+    return wrapper
+
+
+@do_it_twice
+def say_word(word):
+    print('функция отработала', word)
+
+
+say_word("Oo!!!")
+# say_word = do_it_twice(say_word)
+# say_word = do_it_twice(say_word("Aa!!!"))
+print(say_word)
+# say_word("Oo!!!")
+# Oo!!!
+# Oo!!!
